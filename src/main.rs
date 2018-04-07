636d25c54f48e294924eca5598dab6a5d5ed18ec
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-04-07 09:42:10 -0700

    Adds support for cleaning a specific project

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::path::Path;
 use std::process::Command;
 use walkdir::WalkDir;
 
-
 fn main() {
     let args: Vec<_> = env::args().collect();
 
@@ -21,7 +20,10 @@ fn main() {
     } else {
         let term = &args[1];
         let project = &args[2];
-        clean_project(term, project);
+        let mut root = env::current_dir().unwrap();
+        root.push(term);
+        root.push(project);
+        clean_it(&Path::new(&root));
     }
 }
 
@@ -38,38 +40,6 @@ fn clean_it(path: &Path) {
     }
 }
 
-// fn clean_term(term: &String) {
-//     println!("Cleaning up for the term {:?}", term);
-//     // Don't care about path not existing because if the current directory
-//     // doesn't exist, we can't very well clean up...
-//     let mut current_dir = env::current_dir().unwrap();
-//     current_dir.push(term);
-//     let term_root = Path::new(&current_dir);
-
-//     match env::set_current_dir(&term_root) {
-//         Ok(_) => clean_projects_in_term(term_root),
-//         Err(_) => panic!("Couldn't `cd` to {:?}", term),
-//     }
-// }
-
-// fn clean_projects_in_term(term: &Path) {
-//     // iterates over all entries and ignores any errors
-//     for entry in WalkDir::new(term.to_str().unwrap())
-//         .max_depth(4) // should be the max depth from term to dist
-//         .into_iter()
-//         .filter_map(|e| e.ok())
-//     {
-//         if entry.file_type().is_dir() && entry.path().to_str().unwrap().ends_with("dist") {
-//             println!("{}", entry.path().display());
-//             make_clean(entry.path());
-//         }
-//     }
-// }
-
-fn clean_project(term: &String, project: &String) {
-    println!("Cleaning up project {:?} for term {:?}", project, term);
-}
-
 fn make_clean(d: &Path) {
     let status = Command::new("make")
         .current_dir(&d)
@@ -77,5 +47,9 @@ fn make_clean(d: &Path) {
         .status()
         .expect(&format!("unable to `make clean` in {}", d.display()));
 
-    println!("`make clean` in {} exited with status {}", d.display(), status);
+    println!(
+        "\n\t`make clean` in {} exited with status {}\n\n",
+        d.display(),
+        status
+    );
 }

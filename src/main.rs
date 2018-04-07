bdb1892d353c6193fca7341f542c56dd04741d7c
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-04-07 09:34:48 -0700

    A big refactor!

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@ extern crate walkdir;
 
 use std::env;
 use std::path::Path;
+use std::process::Command;
 use walkdir::WalkDir;
+
 
 fn main() {
     let args: Vec<_> = env::args().collect();
 
     if args.len() == 1 {
         println!("Usage: clean-it Term [Project]");
+        println!("\tTerm should be a path to a term (e.g. ~/Winter2018)");
         println!("\nExamples:\n\tclean-it Winter2018\n\tclean-it Winter2018 P2");
     } else if args.len() == 2 {
         let term = &args[1];
-        clean_term(term);
+        let mut root = env::current_dir().unwrap();
+        root.push(term);
+        clean_it(&Path::new(&root));
     } else {
         let term = &args[1];
         let project = &args[2];
@@ -20,33 +25,55 @@ fn main() {
     }
 }
 
-fn clean_term(term: &String) {
-    println!("Cleaning up for the term {:?}", term);
-    // Don't care about path not existing because if the current directory
-    // doesn't exist, we can't very well clean up...
-    let mut current_dir = env::current_dir().unwrap();
-    current_dir.push(term);
-    let term_root = Path::new(&current_dir);
-
-    match env::set_current_dir(&term_root) {
-        Ok(_) => clean_projects_in_term(term_root),
-        Err(_) => panic!("Couldn't `cd` to {:?}", term),
-    }
-}
-
-fn clean_projects_in_term(term: &Path) {
-    // iterates over all entries and ignores any errors
-    for entry in WalkDir::new(term.to_str().unwrap())
+fn clean_it(path: &Path) {
+    for entry in WalkDir::new(path.to_str().unwrap())
         .max_depth(4) // should be the max depth from term to dist
         .into_iter()
         .filter_map(|e| e.ok())
     {
         if entry.file_type().is_dir() && entry.path().to_str().unwrap().ends_with("dist") {
             println!("{}", entry.path().display());
+            make_clean(entry.path());
         }
     }
 }
 
+// fn clean_term(term: &String) {
+//     println!("Cleaning up for the term {:?}", term);
+//     // Don't care about path not existing because if the current directory
+//     // doesn't exist, we can't very well clean up...
+//     let mut current_dir = env::current_dir().unwrap();
+//     current_dir.push(term);
+//     let term_root = Path::new(&current_dir);
+
+//     match env::set_current_dir(&term_root) {
+//         Ok(_) => clean_projects_in_term(term_root),
+//         Err(_) => panic!("Couldn't `cd` to {:?}", term),
+//     }
+// }
+
+// fn clean_projects_in_term(term: &Path) {
+//     // iterates over all entries and ignores any errors
+//     for entry in WalkDir::new(term.to_str().unwrap())
+//         .max_depth(4) // should be the max depth from term to dist
+//         .into_iter()
+//         .filter_map(|e| e.ok())
+//     {
+//         if entry.file_type().is_dir() && entry.path().to_str().unwrap().ends_with("dist") {
+//             println!("{}", entry.path().display());
+//             make_clean(entry.path());
+//         }
+//     }
+// }
+
 fn clean_project(term: &String, project: &String) {
     println!("Cleaning up project {:?} for term {:?}", project, term);
+}
+
+fn make_clean(d: &Path) {
+    Command::new("make")
+            .current_dir(&d)
+            .arg("clean")
+            .spawn()
+            .expect(&format!("unable to `make clean` in {}", d.display()));
 }
